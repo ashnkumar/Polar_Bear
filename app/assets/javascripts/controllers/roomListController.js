4e56ae319ace37bd2db196346ccca2d68058a39b
Apprentice <apprentice@dbc05.local>
2014-05-05 18:52:36 -0700

    back to working version

--- app/assets/javascripts/controllers/roomListController.js
+++ app/assets/javascripts/controllers/roomListController.js
@@ -9,7 +9,7 @@ RoomListApp.RoomListController.prototype = {
 
     $('.room-list').on("click", function(e) {
       if ($(event.target) && $(event.target).hasClass("individual_room")) {
-        self.sendInfoToChatRoom($(event.target).data('id'));
+        this.sendInfoToChatRoom($(event.target).data('id'));
       }
     }.bind(this))
 
