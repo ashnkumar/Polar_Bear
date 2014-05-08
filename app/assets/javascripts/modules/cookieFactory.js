6ca6b9451949f61c48a5dcefbbb6a1163a463836
Bo Keopaseut <Keopaseutb01@hotmail.com>
2014-05-07 20:47:57 -0700

    update to cookieFactory to make all the cookie data show i.e. expire and path

--- app/assets/javascripts/modules/cookieFactory.js
+++ app/assets/javascripts/modules/cookieFactory.js
@@ -12,7 +12,7 @@ var cookieFactory =(function(){
     var days = days || 100
     var date = new Date();
     date.setTime(date.getTime() + (days*24*60*60*1000));
-    var expires = "; expires="+date.toGMTString();
+    var expires = ":expires="+date.toGMTString();
     return expires
   }
 
@@ -20,7 +20,7 @@ var cookieFactory =(function(){
   var _createCookie = function(userLatitude, userLongitude, days){
     var userToken = _createUserToken();
     var date = _createDate(days);
-    var cookieString = "user-token=" + userToken + ":user-Latitude=" + userLatitude + ":user-Longitude=" + userLongitude + date + "; path=/" ;
+    var cookieString = "user-token=" + userToken + ":user-Latitude=" + userLatitude + ":user-Longitude=" + userLongitude + date + ": path=/" ;
     document.cookie = cookieString
 }
 
